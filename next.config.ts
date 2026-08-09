94acbc32d26f08644bb317f793d060d2e9e03fa7
Aljayz <albertjaybarororosique@gmail.com>
2026-08-09 18:12:42 +0800

    Update basePath in next.config.ts to remove leading slash

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   // Add the line below. Use the EXACT name of your new repository.
-  basePath: "/", 
+  basePath: "", 
   images: {
     unoptimized: true,
     remotePatterns: [
